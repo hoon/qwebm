f7678e7e1d917efbf47e03c6685e5e350d5a82a0
Hoon Choi <hoon@orcatown.com>
2026-01-14 23:59:20 -0500

    set gop size

--- qwebm.py
+++ qwebm.py
@@ -19,6 +19,7 @@ MD_LONG = 960  # medium size video dimension of the long side (usually width)
 MD_TARGET_SIZE_KB = 6 * 1024  # medium size video target size
 MD_CRF = 9
 MD_AUDIO_QSCALE = 3
+MD_GOP_SIZE = 120  # group of pictures, keyframes are placed every gop_size frames, may fix tracking on some players
 
 MAX_TRY = 5
 MAX_BIT_RATE_MULT = 4
@@ -324,6 +325,9 @@ def generate_ffmpeg_options(
 
     options.append("-crf")
     options.append(f"{MD_CRF + crf_adjust}")
+
+    options.append("-g")
+    options.append(f"{MD_GOP_SIZE}")
 
     if include_audio:
         options.append("-c:a")
